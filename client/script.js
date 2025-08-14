6e44d496253fff6a8c33ee8aeb32732159a6d798
SrStamm <mirkostamm19@gmail.com>
2025-08-14 17:44:41 +0100

    Refactor login and registration logic; improve error handling and user feedback

--- client/script.js
+++ client/script.js
@@ -1,9 +1,16 @@
-// Instancias
+// URL del servidor
+const url = "http://localhost:8000";
+
+// Botones
 let showRegisterBtn = document.getElementById("showRegister");
+let showLoginBtn = document.getElementById("showLogin");
+let logoutBtn = document.getElementById("logoutBtn");
+let userBtn = document.getElementById("userBtn");
+
+// Formularios
 let registerForm = document.getElementById("registerForm");
 let registerElement = document.getElementById("registerFormElement");
 
-let showLoginBtn = document.getElementById("showLogin");
 let loginForm = document.getElementById("loginForm");
 let loginElement = document.getElementById("loginFormElement");
 
@@ -12,6 +19,13 @@ let notesSection = document.getElementById("notesSection");
 let messageContainer = document.getElementById("messageContainer");
 
 // Funciones
+function loginAcccessSuccess() {
+  loginForm.style.display = "none";
+  notesSection.style.display = "block";
+  logoutBtn.style.display = "block";
+  userBtn.style.display = "block";
+}
+
 showRegisterBtn.addEventListener("click", () => {
   loginForm.style.display = "none";
   registerForm.style.display = "block";
@@ -24,6 +38,11 @@ showLoginBtn.addEventListener("click", () => {
 
 // Mostrar advertencia personalizada
 function showMessage(message, type = "error") {
+  // Vacia por si habia un mensaje antes
+  messageContainer.style.display = "none";
+  messageContainer.textContent = "";
+
+  // Actualiza el mensaje y lo muestra
   messageContainer.textContent = message;
   messageContainer.className = `alert alert-${type}`;
   messageContainer.style.display = "block";
@@ -31,11 +50,11 @@ function showMessage(message, type = "error") {
   setTimeout(() => {
     messageContainer.style.display = "none";
     messageContainer.textContent = "";
-  }, 5000);
+  }, 4500);
 }
 
 // Enviar formulario
-registerElement.addEventListener("submit", function (event) {
+registerElement.addEventListener("submit", async (event) => {
   event.preventDefault();
 
   const datos = {
@@ -44,36 +63,42 @@ registerElement.addEventListener("submit", function (event) {
     password: document.getElementById("newPassword").value,
   };
 
-  fetch("http://localhost:8000/users/", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(datos),
-  })
-    .then((response) => {
-      if (!response.ok) {
-        return response.json().then((err) => {
-          throw new Error(err.detail || "Error al enviar el formulario");
-        });
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log("Respuesta del servidor:", data);
-
-      registerForm.style.display = "none";
-      loginForm.style.display = "block";
-    })
-    .catch((error) => {
-      console.error("Hubo un problema con la operación fetch:", error.message);
-      showMessage(`Error: ${error.message}`, "error");
+  try {
+    const response = await fetch(url + "/users/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(datos),
     });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.detail || "Error al enviar el formulario");
+    }
+
+    const data = await response.json();
+    console.log("Respuesta del servidor:", data);
+
+    showMessage(
+      "Usuario creado exitosamente! Ya puedes acceder al servicio.",
+      "success",
+    );
+    registerForm.style.display = "none";
+    loginForm.style.display = "block";
+  } catch (error) {
+    console.error("Hubo un problema con la operación fetch:", error.message);
+    showMessage(`Error: ${error.message}`, "error");
+  }
 });
 
 loginElement.addEventListener("submit", async (event) => {
   event.preventDefault();
 
+  // Elimina el token almacenado, si existe
+  localStorage.removeItem("authToken");
+
+  // Obtiene la información del formulario
   const username = document.getElementById("username").value;
   const password = document.getElementById("current-password").value;
 
@@ -82,6 +107,7 @@ loginElement.addEventListener("submit", async (event) => {
   formData.append("username", username);
   formData.append("password", password);
 
+  // Realiza la llamada al servidor
   try {
     const response = await fetch("http://localhost:8000/login", {
       method: "POST",
@@ -104,8 +130,8 @@ loginElement.addEventListener("submit", async (event) => {
       localStorage.setItem("authToken", data.access_token);
       showMessage("Inicio de sesión exitoso!", "success");
 
-      loginForm.style.display = "none";
-      notesSection.style.display = "block";
+      loginAcccessSuccess();
+      showNotes();
     } else {
       showMessage("Error: Problemas con el Servidor", "error");
     }
@@ -114,3 +140,74 @@ loginElement.addEventListener("submit", async (event) => {
     showMessage(`Error: ${error.message}`, "error");
   }
 });
+
+logoutBtn.addEventListener("click", async () => {
+  token = localStorage.getItem("authToken");
+
+  try {
+    const response = await fetch(url + "/logout", {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + token,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.detail || "Error al cerrar sesión");
+    }
+
+    loginForm.style.display = "block";
+    notesSection.style.display = "none";
+    logoutBtn.style.display = "none";
+    userBtn.style.display = "none";
+
+    showMessage("Sesión cerrada correctamente", "success");
+  } catch (error) {
+    console.error("Error al cerrar sesión:", error);
+    showMessage(`Error: ${error.message}`, "error");
+  }
+});
+
+// Notas
+async function showNotes() {
+  const token = localStorage.getItem("authToken");
+
+  try {
+    const response = await fetch(url + "/notes/personal/", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + token,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.detail || "Error al obtener las notas");
+    }
+
+    // Obtiene las notas
+    let listNotes = await response.json();
+
+    let notesContainer = document.getElementById("notesContainer");
+
+    // Obtiene el template de la nota
+    const noteTemplate = document.getElementById("noteTemplate");
+
+    listNotes.forEach((notes) => {
+      const clonTemplate = noteTemplate.content.cloneNode(true);
+
+      // Accede a cada parte del template
+      const textNote = clonTemplate.querySelector(".note-text");
+      const categoryNote = clonTemplate.querySelector("#noteCategoryBadge");
+
+      // Actualiza
+      textNote.textContent = notes.text;
+      categoryNote.textContent = notes.category;
+
+      notesContainer.appendChild(clonTemplate);
+    });
+  } catch (error) {}
+}
