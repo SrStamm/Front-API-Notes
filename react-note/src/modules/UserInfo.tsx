61f49ab2aadfe088732c951439eeb130c6d57a0c
SrStamm <mirkostamm19@gmail.com>
2025-10-30 16:49:23 +0000

    feat: implemented get Session data for user

---
+++ react-note/src/modules/UserInfo.tsx
@@ -0,0 +1,124 @@
+import { useCallback, useEffect, useState } from "react";
+import Fetch from "../utils/api";
+import { useNavigate } from "react-router-dom";
+
+interface sessionData {
+  session_id: string;
+  is_active: boolean;
+  user_id: number;
+}
+
+interface userDataInterface {
+  username: string;
+}
+
+function UserInfo() {
+  const navigate = useNavigate();
+
+  const handleInvalidToken = useCallback(() => {
+    localStorage.removeItem("auth_token");
+    navigate("/login");
+  }, [navigate]);
+
+  const [currentInfo, setCurrentInfo] = useState<sessionData[]>([]);
+  const [currentUser, setCurrentUser] = useState<userDataInterface>({
+    username: "",
+  });
+
+  const getSession = useCallback(async (): Promise<sessionData[]> => {
+    try {
+      const response = await Fetch({
+        path: "sessions",
+        method: "GET",
+      });
+
+      if (response.ok) {
+        const sessions = await response.json();
+        setCurrentInfo(sessions);
+        return sessions;
+      } else if (response.status === 401) {
+        handleInvalidToken();
+        return [];
+      } else {
+        const errorData = await response.json();
+        throw new Error(errorData.detail);
+      }
+    } catch (error) {
+      console.error("Error: ", error);
+      return [];
+    }
+  }, [handleInvalidToken, setCurrentInfo]);
+
+  const getCurrentUserInfo =
+    useCallback(async (): Promise<userDataInterface> => {
+      try {
+        const response = await Fetch({
+          path: "users/me",
+          method: "GET",
+        });
+
+        if (response.ok) {
+          const user = await response.json();
+          setCurrentUser(user);
+          return user;
+        } else if (response.status === 401) {
+          handleInvalidToken();
+          return { username: "" };
+        } else {
+          const errorData = await response.json();
+          throw new Error(errorData.detail);
+        }
+      } catch (error) {
+        console.error("Error: ", error);
+        return { username: "" };
+      }
+    }, [handleInvalidToken, setCurrentUser]);
+
+  useEffect(() => {
+    getSession();
+    getCurrentUserInfo();
+  }, [getSession, getCurrentUserInfo]);
+
+  return (
+    <section className="section">
+      <div className="section-header">
+        <h1>Perfil</h1>
+      </div>
+      <div className="">
+        {currentUser ? (
+          <h2>Nombre: {currentUser.username}</h2>
+        ) : (
+          <p>Cargando información del usuario...</p>
+        )}
+      </div>
+      <div className="table-responsive">
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Activo</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {currentInfo.map((session) => (
+              <tr key={session.session_id}>
+                <td>{session.session_id}</td>
+                {session.is_active === true ? (
+                  <td> Activo </td>
+                ) : (
+                  <td> Inactivo </td>
+                )}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div>
+        <button className="btn btn-danger"> Cerrrar todas las sesiones</button>
+      </div>
+    </section>
+  );
+}
+
+export default UserInfo;
